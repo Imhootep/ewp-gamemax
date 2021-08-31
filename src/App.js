b6ae451aba11981f7e5c007fba7602bb5311914b
Imhootep <fpbraibant@gmail.com>
2021-08-31 16:29:09 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from 'react';
+import logo from './img/logo2.png';
+import controller from './img/gamepad.png'
+import LoginForm from './components/LoginForm';
+
+
 
 function App() {
+
+    const adminUser ={
+      email:"fb@admin.com",
+      password:"test"
+    }
+
+    const [user, setUser] = useState ({name:"", email:""});
+    const [error, setError]= useState("");
+
+    const Login = details => {
+      console.log(details);
+
+      if(details.email === adminUser.email && details.password === adminUser.password){
+        console.log("Logged in")
+        setUser({
+          email:details.email,
+          password:details.password
+        })
+      } else {
+        console.log("Details do not match")
+        setError("Email ou Mot de passe incorrect" )
+      }
+    }
+
+    const Logout =()=>{
+      setUser({name:"", email:""});
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {(user.email !=="") ?(
+          <div className="welcome">
+            <h2>Welcome, <span>{user.email}</span> </h2>
+            <button onClick={Logout}>Logout</button>
+          </div>
+      ) : (
+        <header className="App-header">
+        <div className="logo">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <img src={controller} className="App-controller" alt="controller" />
+        </div>
+          <LoginForm Login={Login} error={error}/>
+
+          </header>
+      )}
+      
+
+        </div>
+       
   );
 }
 
